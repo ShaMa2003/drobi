d25fed8889c6f8ce3ee089d648e345ee924fdd52
rotoyouoi <rotoyouoi@yandex.ru>
2022-12-05 16:32:25 +0500

    Добавьте файлы проекта.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using drobi;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string stroka = "9/10";
+            string expected = "9/10";
+
+            FixWrongResult g = new FixWrongResult();
+            string actual = g.FixWrongResultt(stroka);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string stroka = "15/10";
+            string expected = "10/10";
+
+            FixWrongResult g = new FixWrongResult();
+            string actual = g.FixWrongResultt(stroka);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            string stroka = "-10/10";
+            string expected = "-10/10";
+
+            FixWrongResult g = new FixWrongResult();
+            string actual = g.FixWrongResultt(stroka);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            string stroka = "12/-10";
+            string expected = "-10/-10";
+
+            FixWrongResult g = new FixWrongResult();
+            string actual = g.FixWrongResultt(stroka);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            string stroka = "";
+          
+
+            FixWrongResult g = new FixWrongResult();
+            Action actual =()=> g.FixWrongResultt(stroka);
+            Assert.ThrowsException<Exception>(actual);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            string stroka = "10/1b";
+            string expected = "Error!";
+
+            FixWrongResult g = new FixWrongResult();
+            string actual = g.FixWrongResultt(stroka);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ drobi/FixWrongResult.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drobi
+{
+    public class FixWrongResult
+    {
+        public string FixWrongResultt(string stroka)
+        {
+            string[] subs = stroka.Split('/');
+            string str = "";
+            //if (subs{[0], subs[1] != )
+            //{
+
+            //}
+            if (stroka == "")
+            {
+                    throw new Exception("Error!");
+            }
+                else
+                {
+                    if (Convert.ToDouble(subs[0]) > Convert.ToDouble(subs[1]))
+                    {
+                        subs[0] = subs[1];
+                    }
+                    str = String.Join("/", subs);
+                }
+            return str;
+        }
+    }
+}
